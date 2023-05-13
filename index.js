683346e62a1f76b129d7319ffd59db8e3450f5d5
aminul-islam01 <abiddut953@gmail.com>
2023-05-13 12:33:24 +0600

    add jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -22,6 +23,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    // console.log(req.headers.authorization)
+    const authorization = req.headers.authorization;
+    if(!authorization) {
+        return res.status(401).send({error: true, message: 'unauthorize access'})
+    }
+    const token = authorization.split(' ')[1];
+    // console.log(token)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({error: true, message: 'unauthorize access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +48,15 @@ async function run() {
         const serviceCollection = client.db('carDoctorDB').collection('services');
         const bookingCollection = client.db('carDoctorDB').collection('bookings');
 
+        // JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            res.send({token})
+        })
+
+        // services routes
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -48,8 +75,15 @@ async function run() {
             res.send(result);
         })
 
-        // bookings
-        app.get('/bookings', async(req, res) => {
+        // bookings routes
+        app.get('/bookings', verifyJWT, async(req, res) => {
+            const decoded = req.decoded;
+            console.log('booking de', decoded)
+
+            if(decoded.email !== req.query.email) {
+                return res.status(403).send({error: 1, message: 'forbidden access'})
+            }
+
             let query = {};
             if(req.query?.email) {
                 query = {email: req.query.email}
